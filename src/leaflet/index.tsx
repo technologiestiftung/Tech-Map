091447d475b4bc47a9c1766b2c0234b049363c37
Urs Schmidt <urs.schmidt@ts.berlin>
2023-01-18 09:29:14 +0100

    🤼‍♀️ Remove double import

--- src/leaflet/index.tsx
+++ src/leaflet/index.tsx
@@ -12,9 +12,8 @@ import { createRef, FC, useState } from 'react'
 
 import { TestSlider } from './TestSlider'
 import { MarkerCloud } from './MarkerCloud'
-import { baseLayer, middelRing, innerRing, frameworkLine, lineData } from './polygonData'
+import { lineData } from './polygonData'
 import { TestMarker } from './TestMarker'
-import { PolyLineComponent } from './PolyLine'
 import { Infobox } from './Infobox'
 import { SVGPathes } from './svgs/SVGPathes'
 
